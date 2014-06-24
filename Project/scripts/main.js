e16f7032cb14af4582734775889c840f881cf149
lmaestri <luca.maestri@hotmail.de>
2014-06-24 23:30:39 +0200

    removed bugs and some lines of code

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -1,102 +1,67 @@
 
 var map;
 var pos;
-var goldStar = {
-    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-    fillColor: 'yellow',
-    fillOpacity: 0.8,
-    scale: 0.1,
-    strokeColor: 'gold',
-    strokeWeight: 1
-  };
-  
-var marker = new google.maps.Marker({
-		icon: goldStar,
-		map: map
-	  });
+var infoParagraph;
 
-function initialize() {
-
-  var mapOptions = {
+var mapOptions = {
     zoom: 18,
     disableDefaultUI: true
-  };
+};
+
+var circle = new google.maps.Circle({
+  strokeColor: '#0000FF',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#0000FF',
+  fillOpacity: 0.35,
+  map: map,
+  clickable: false,
+  radius: 30
+});
+
+var marker = new google.maps.Marker({
+      map: null,
+      title: 'Player'
+});
+
+function initialize() {
+  infoParagraph = document.getElementById('info');
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      
-	 /* var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
-      });
-	  */
-	map.setCenter(pos);
-
-
-	marker.setPosition(pos);
-	
-	var circle = {
-      strokeColor: '#0000FF',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#0000FF',
-      fillOpacity: 0.35,
-      map: map,
-	  radius: 30
-  };
-
-
-	var radius = new google.maps.Circle(circle);
-	radius.bindTo('center', marker, 'position');
-
-	  
-	  //addMarker(goldstar);
-	  
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
-  
+  circle.setMap(map);
   watchPosition();
-  
 }
 
 function watchPosition(){
 	if(navigator.geolocation) {
-		navigator.geolocation.watchPosition(setPosition, handleNoGeolocation);
-	}
+		navigator.geolocation.getCurrentPosition(setPosition, showError);
+	}else{
+    writeInfo('Browser does not support geolocation.');
+  }
 }
 
 function setPosition(position){
   pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  var x = document.getElementById("pos");
-  x.innerHTML="Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-  marker.setPosition(pos);
+  map.panTo(pos);
+  circle.setCenter(pos);
+  alert(circle.center)
+  writeInfo(position);
 }
 
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
+function showError(error) {
+  switch(error.code) {
+      case error.PERMISSION_DENIED:
+          writeInfo("User denied the request for Geolocation.");
+          break;
+      case error.POSITION_UNAVAILABLE:
+          writeInfo("Location information is unavailable.");
+          break;
+      case error.TIMEOUT:
+          writeInfo("The request to get user location timed out.");
+          break;
+      case error.UNKNOWN_ERROR:
+          writeInfo("An unknown error occurred.");
+          break;
   }
-
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(60, 105),
-    content: content
-  };
-
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
 }
 
 function addMarker(icon){
@@ -105,6 +70,14 @@ function addMarker(icon){
 		icon: icon,
 		map: map
   });
+}
+
+function writeInfo(content){
+  infoParagraph.innerHTML = content;
+}
+
+function writeInfo(position){
+  infoParagraph.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
