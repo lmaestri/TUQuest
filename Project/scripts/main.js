a977947a3fc3b5f34013acc2a7dd02bb3c28ba6d
lmaestri <luca.maestri@hotmail.de>
2014-06-25 00:19:26 +0200

    added player/chest icon
    player and chest icons

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -3,6 +3,40 @@ var map;
 var pos;
 var infoParagraph;
 
+var playerIcon = {
+  url : 'images/player_small.png',
+  size : new google.maps.Size(32,32),
+  origin: new google.maps.Point(0,0),
+  anchor: new google.maps.Point(16, 16)
+};
+
+var chestIcon = {
+  url : 'images/chest.png',
+  size : new google.maps.Size(32,32),
+  origin: new google.maps.Point(0,0),
+  anchor: new google.maps.Point(16, 16)
+};
+
+var markerTypes = {};
+
+markerTypes['player'] = {
+  map : null,
+  icon : playerIcon,
+  title : 'Player',
+  zIndex : 1
+};
+
+markerTypes['chest'] = {
+  map : null,
+  icon : chestIcon,
+  title : 'Chest',
+  zIndex : 2
+};
+
+var playerMarker;
+
+var markersArray = [];
+
 var mapOptions = {
     zoom: 18,
     disableDefaultUI: true
@@ -19,15 +53,14 @@ var circle = new google.maps.Circle({
   radius: 30
 });
 
-var marker = new google.maps.Marker({
-      map: null,
-      title: 'Player'
-});
-
 function initialize() {
   infoParagraph = document.getElementById('info');
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   circle.setMap(map);
+  
+  playerMarker = new google.maps.Marker(markerTypes['player']);
+  playerMarker.setMap(map);
+
   watchPosition();
 }
 
@@ -43,7 +76,9 @@ function setPosition(position){
   pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   map.panTo(pos);
   circle.setCenter(pos);
-  alert(circle.center)
+  playerMarker.setPosition(pos);
+  chestPos = new google.maps.LatLng(position.coords.latitude - 0.000001, position.coords.longitude);
+  addMarker(markerTypes['chest'], )
   writeInfo(position);
 }
 
@@ -64,20 +99,47 @@ function showError(error) {
   }
 }
 
-function addMarker(icon){
-	var marker = new google.maps.Marker({
-		position: map.getCenter(),
-		icon: icon,
-		map: map
-  });
-}
-
 function writeInfo(content){
   infoParagraph.innerHTML = content;
 }
 
 function writeInfo(position){
   infoParagraph.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+function addMarker(location, markerType) {
+  marker = new google.maps.Marker(markerType);
+  marker.setMap(map);
+  marker.setPosition(location);
+  markersArray.push(marker);
+}
+
+// Removes the overlays from the map, but keeps them in the array
+function clearOverlays() {
+  if (markersArray) {
+    for (i in markersArray) {
+      markersArray[i].setMap(null);
+    }
+  }
+}
+
+// Shows any overlays currently in the array
+function showOverlays() {
+  if (markersArray) {
+    for (i in markersArray) {
+      markersArray[i].setMap(map);
+    }
+  }
+}
+
+// Deletes all markers in the array by removing references to them
+function deleteOverlays() {
+  if (markersArray) {
+    for (i in markersArray) {
+      markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
+  }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
